1ce104b496158e3010564f375340405f5172f16b
Justin Justin <no@no.com>
2019-05-15 20:18:24 -0500

    starting plot 6

---
+++ plot6.R
@@ -0,0 +1,58 @@
+#code for week 4 project
+#librarys
+install.packages("dplyr")
+install.packages("purrr")
+library(dplyr)
+library(ggplot2)
+options(scipen=999)
+#This is how you load the file in regular R 
+#source("filename")
+
+
+
+
+#What you will need to do
+#Combine 
+#Across the United States, how have emissions from 
+#coal combustion-related sources changed from 1999–2008?
+
+mySetup<-function(){
+	temp<-tempfile()
+	download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",temp)
+	unzip(temp)
+	unlink(temp)
+}
+myReadData<-function(){
+	NEI<<-readRDS("summarySCC_PM25.rds")
+	SCC<<-readRDS("Source_Classification_Code.rds")
+	myFull<<-merge(NEI, SCC,by="SCC",all=TRUE)
+}
+
+myPrepData<-function(){
+  #filter for Vehicle
+  myComb<<-myFull[grep("vehicle",myFull$SCC.Level.Two, ignore.case=TRUE),]
+  myComb<<-subset(myComb,fips=="24510")
+  myCombS<<-summarise(group_by(myComb, year),Emissions=sum(Emissions))
+  #View(myComb)
+}
+
+myCreatePNG<-function(){
+
+	 	 
+	 
+	 png("plot5.png", width=480, height=480)
+	 g<-ggplot(myCombS,aes(year, Emissions))
+	 g<-g+geom_point()
+	 g<-g+geom_smooth(method="lm")
+	 print(g)
+	 dev.off()
+}
+
+
+
+
+#Run these scripts
+#mySetup()
+#myReadData()
+myPrepData()
+myCreatePNG()
